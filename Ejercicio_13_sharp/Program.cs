2d89059e459bc1b98d3b7241916ecd8b4880eea8
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-10-29 13:21:59 +0200

    Agregada la función para el intercambio de valores entre variables y la muesta del resultado por pantalla.

--- Ejercicio_13_sharp/Program.cs
+++ Ejercicio_13_sharp/Program.cs
@@ -6,7 +6,20 @@ namespace Ejercicio_13_sharp
     {
         static void Main(string[] args)
         {
-            
+            int auxiliar;
+
+            Console.Write("Introduzca un número entero: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduzca otro número entero: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            auxiliar = numero1;
+            numero1 = numero2;
+            numero2 = auxiliar;
+
+            Console.WriteLine("\nVariable 1: " + numero1);
+            Console.WriteLine("Variable 2: " + numero2);
+
         }
     }
 }
